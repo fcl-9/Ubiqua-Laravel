7b00d6e734547d48e21986a38c18c7168dab047b
fcl-93 <fabiocl93@gmail.com>
2017-04-21 15:06:59 +0100

    Todo List - Division

--- resources/views/shopping_list.blade.php
+++ resources/views/shopping_list.blade.php
@@ -3,8 +3,24 @@
 
 @section('inner-css')
 <style>
+    #app{
+        height: 100%;
+    }
     .container-head{
         background-color: #ed9c9b;
+    }
+    .row{
+        margin:0;
+        height:calc(100% - 132px);
+    }
+    .col-md-6{
+        height: 100%;
+    }
+    .font-shop-list{
+        font-weight: bold;
+        font-size: 20px;
+        display:block;
+        text-align: center;
     }
 </style>
 @endsection
@@ -14,6 +30,14 @@
         <img width="40" height="40" src="/img/g4.png">
         <span class="font-header"> To Buy </span>
     </div>
-    <hr>
+    <div style="margin-top: 20px;"></div>
+    <div class="row">
+        <div class="col-md-6">
+            <span class="font-shop-list">Shopping List</span>
+        </div>
+        <div class="col-md-6">
+            <span class="font-shop-list">Add New Products</span>
+        </div>
+    </div>
 @endsection
 
